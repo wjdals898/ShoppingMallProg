66c210f117afa7edee96b222a6ba32202144e5a5
wjdals898 <jungmin898@naver.com>
2021-12-06 22:12:15 +0900

    django-crispy-forms 적용

--- ShoppingMallProg/settings.py
+++ ShoppingMallProg/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
+    'crispy_forms',
 
 
     'mall',
@@ -131,6 +131,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
