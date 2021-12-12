03f6a7a0980fca5c9f009832440aee517040d417
wjdals898 <jungmin898@naver.com>
2021-12-13 01:14:58 +0900

    pagination으로 상품 나누어 보여 주기

--- mall/views.py
+++ mall/views.py
@@ -46,6 +46,7 @@ def new_comment(request, pk):
 class ProductList(ListView):
     model = Product
     ordering = '-pk'
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductList, self).get_context_data()
