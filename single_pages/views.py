4c0ce81bd38ba56cd2cda4893d0676b40a38bec0
wjdals898 <jungmin898@naver.com>
2021-12-13 20:07:06 +0900

    홈페이지 수정

--- single_pages/views.py
+++ single_pages/views.py
@@ -3,9 +3,14 @@ from mall.models import Product
 
 # Create your views here.
 def landing(request):
-    #recent_posts = Product.objects.order_by('-pk')[:3]
-    return render(request, 'single_pages/landing.html')
-                  #,{'recent_posts' : recent_posts})
+    new_products = Product.objects.order_by('-pk')[:3]
+    return render(
+        request,
+        'single_pages/landing.html',
+        {
+            'new_products': new_products,
+        }
+    )
 
 def about_company(request):
     return render(request, 'single_pages/about_company.html')
